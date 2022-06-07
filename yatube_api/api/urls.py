0fb8085d05bc7b7d729f09c2da17d36594d8e000
Новиков Дмитрий <Simplate@bk.ru>
2022-06-07 17:41:22 +0300

    Done

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,22 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router_v1 = DefaultRouter()
+
+router_v1.register('posts', PostViewSet, basename='posts')
+router_v1.register('follow', FollowViewSet, basename='follow')
+router_v1.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+                   basename='comments')
+router_v1.register('groups', GroupViewSet)
 
 urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -11,6 +11,13 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='posts_group',
+        verbose_name='Сообщество'
+    )
 
     def __str__(self):
         return self.text
@@ -24,3 +31,35 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200,
+                             verbose_name='Имя'
+                             )
+    slug = models.SlugField(unique=True,
+                            verbose_name='Название группы'
+                            )
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='follower',
+        on_delete=models.CASCADE,
+    )
+    following = models.ForeignKey(
+        User,
+        related_name='following',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        models.UniqueConstraint(
+            name='unique_follow',
+            fields=['user', 'author']
+        )
